deaad33ab7ec4f8d27811308edd6aa39179c4a90
daineee424 <daineee424@gmail.com>
2025-01-09 17:46:58 +0900

    Feat: 채팅 상품별로 주소 매핑

--- src/main/java/com/secondhand/controller/product/ProductChattingServlet.java
+++ src/main/java/com/secondhand/controller/product/ProductChattingServlet.java
@@ -1,11 +1,22 @@
 package com.secondhand.controller.product;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.secondhand.model.dto.attachment.Attachment;
+import com.secondhand.model.dto.member.Member;
+import com.secondhand.model.dto.product.ProductDetail;
+import com.secondhand.model.service.attachment.AttachmentService;
+import com.secondhand.model.service.member.MemberService;
+import com.secondhand.model.service.member.WishListService;
+import com.secondhand.model.service.product.ProductService;
 
 /**
  * Servlet implementation class ProductChatting
@@ -26,6 +37,27 @@ public class ProductChattingServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String productNo = request.getParameter("productNo");
+		
+		if(productNo != null && !productNo.isEmpty()) {
+			ProductService productService = new ProductService();
+			
+			ProductDetail product = productService.selectProductDetailByProductNo(productNo);
+			List<Attachment> attachments = new AttachmentService().selectAttachmentsByProductNo(productNo);
+			Map<String, Object> sellerInfo = new MemberService().getSellerInfoByMemberNo(product.getProductMemberNo());
+			
+			//좋아요 체크 구문.
+			Member loginMember=(Member)request.getSession().getAttribute("loginMember");
+			if(loginMember!=null) {
+				request.setAttribute("isWishListed", new WishListService().isWishListed(
+						Map.of("memberNo", loginMember.getMemberNo(), "productNo", productNo)));
+			}
+			
+			request.setAttribute("product", product);
+			request.setAttribute("attachments", attachments);
+			request.setAttribute("sellerInfo", sellerInfo);
+		}
+		
 		request.getRequestDispatcher("/WEB-INF/views/product/productChatting.jsp")
 		.forward(request, response);
 	}
